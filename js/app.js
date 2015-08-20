cd72eb6515739db746a3a69a83f19b7aebd3c9ef
Riccardo Salucco <riccardo@notthatred.com>
2015-08-20 19:53:15 +0200

    Add Player and Gem Classes, add variables and instantiate all objects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+// Add Variables
+var userSelections = [false, false, false]; 
+var renderFlag = false;
+var charImages = [
+    'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png'
+    ];
+var charIndex; 
+var enemySpeedMult = 300;
+var collisionProx = 30;
+var lives;
+var gemImages = [
+    'images/Gem-Green.png',
+    'images/Gem-Blue.png',
+    'images/Gem-Orange.png'
+    ];
+var gemIndex;
+var gemX = [0, 100, 200, 300, 400];
+var pointsPerGem;
+var totalPoints = 0;
+var minutes;
+var allY = [60, 143, 226];
+
 // Enemies Class (our player must avoid them!)
 var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
@@ -27,16 +53,78 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player Class
+var Player = function() {
+  this.x = 200;
+  this.y = 400;
+}
 
+Player.prototype.update = function() {
+    if (this.ctlKey === 'left' && this.x > 0){ 
+        this.x = this.x - 100;
+    } else if (this.ctlKey === 'right' && this.x != 400){
+        this.x = this.x + 100;
+    } else if (this.ctlKey === 'up'){
+        this.y = this.y - 83;
+    } else if (this.ctlKey === 'down' && this.y != 400){
+        this.y = this.y + 83;
+    }
+    this.ctlKey = null;
+    if (this.y < 60){
+        this.reset();
+    }
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.render = function() {
+ ctx.drawImage(Resources.get(charImages[charIndex]), this.x, this.y);
+}
 
+Player.prototype.handleInput = function(key) {
+  this.ctlKey = key;
+}
 
+Player.prototype.reset = function() {
+  player.x = 200;
+  player.y = 400;
+  lives--;
+  document.getElementById('lives').innerHTML = 'Lives: ' + lives;
+  if (lives < 0) {
+    endGame();
+  }
+}
+
+// Gem class
+var Gem = function() {
+  this.x = gemX[Math.floor(Math.random() * 5)];
+  this.y = allY[Math.floor(Math.random() * 3)];
+}
+
+Gem.prototype.update = function() {
+    if (player.y === this.y + 8 && player.y === this.y + 8) {
+        if (player.x === this.x && player.x === this.x) {
+        totalPoints = totalPoints + pointsPerGem;
+        this.x = gemX[Math.floor(Math.random() * 5)];
+        this.y = allY[Math.floor(Math.random() * 3)];
+        }
+    }
+    document.getElementById('points').innerHTML = 'Points: ' + totalPoints;
+}
+
+Gem.prototype.render = function() {
+  ctx.drawImage(Resources.get(gemImages[gemIndex]), this.x, this.y);
+
+}
+
+// Instantiate objects.
+var enemyA = new Enemy();
+var enemyB = new Enemy();
+var enemyC = new Enemy();
+var enemyD = new Enemy();
+var allEnemies = [enemyA, enemyB, enemyC, enemyD];
+
+var player = new Player();
+
+var gem = new Gem();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
